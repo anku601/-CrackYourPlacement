84985524856b0bbc7a5d907c3f79883c81cb732a
Ankan Mondal <97394038+anku601@users.noreply.github.com>
2024-07-23 01:30:20 +0530

    Find the Duplicate Number

---
+++ findDuplicates.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+         unordered_map<int, int> map;
+        for(int i=0; i<nums.size(); i++) {
+            if(map.find(nums[i]) != map.end()) {
+                return nums[i];
+            } else map[nums[i]] = 1;
+        }
+        return 0;
+     }
+     
+};

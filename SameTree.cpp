20a36fb504778a980fc8e34404c359f5b31bfd91
Ankan Mondal <97394038+anku601@users.noreply.github.com>
2024-07-25 19:12:59 +0530

    Same Tree

---
+++ SameTree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL){
+            return true ;
+        }
+
+        if(p==NULL && q!= NULL){
+            return false;
+        }
+
+        if(p!=NULL && q== NULL){
+            return false;
+        }
+        
+
+        bool left =isSameTree(p->left,q->left);
+
+        bool right =isSameTree(p->right ,q->right);
+
+        bool data= p->val ==q->val;
+
+        if(left && right && data){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};

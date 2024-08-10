acf105cb0a22ef9c95cbc2869801411155b05a3f
Ankan Mondal <97394038+anku601@users.noreply.github.com>
2024-08-10 23:40:47 +0530

    Remove Nth Node From End of List

---
+++ removeNthfromEnd.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head == NULL)
+            return head;
+        if (n == 0)
+            return head;
+
+    // Step 1: Calculate the length of the linked list
+        int length = 0;
+        ListNode* curr = head;
+        while (curr != NULL) {
+            length++;
+            curr = curr->next;
+        }
+    // Step 2: Check if we need to remove the head
+        if (length == n) {
+            curr = head->next;
+            return curr;
+        }
+    // Step 3: Find the position of the node to remove
+        curr = head;
+        int changeFrom = length - n;
+        int count = 1;
+        while (count < changeFrom && curr != NULL) {
+            curr = curr->next;
+            count++;
+        }
+        curr->next = curr->next->next;
+        return head;
+    }
+};
